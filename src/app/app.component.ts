1b15e12943a6ac4ddbd97a1d91c52c4285850396
Pete Gamble <peter.gamble@ideagen.com>
2018-01-24 16:51:14 +0000

    Added Sign In button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string = 'Vale of Belvoir Cycling Club';
+
+  signIn(): void{
+    console.log('clicky'); 
+    window.location.href = 'https://valeofbelvoir.auth.eu-west-2.amazoncognito.com/login?response_type=code&client_id=4arssi7pu1ghutcqcu7qdsv5re&redirect_uri=https://www.valeofbelvoir.cc/user/auth';
+
+  }
 }
